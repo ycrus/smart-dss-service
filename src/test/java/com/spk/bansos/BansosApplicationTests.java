e8838fffa132558934178d541fd703f1d0ac6399
yohanes14 <ycrusc@ist.id>
2025-05-01 19:59:42 +0700

    remove test

--- src/test/java/com/spk/bansos/BansosApplicationTests.java
+++ src/test/java/com/spk/bansos/BansosApplicationTests.java
@@ -1,13 +1,1 @@
-package com.spk.bansos;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class BansosApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
